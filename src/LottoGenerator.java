4992d94fdb72ed18ca6a9a0248ce27e01798a080
Alex Suilea <alexsuilea21@gmail.com>
2026-07-23 20:27:05 +0300

    Updates for 3 tickets

--- src/LottoGenerator.java
+++ src/LottoGenerator.java
@@ -5,6 +5,16 @@ import java.util.stream.IntStream;
 
 public class LottoGenerator  {
     public static void main(String[] args) {
+        List<Integer> ticketA = getTicket();
+        List<Integer> ticketB = getTicket();
+        List<Integer> ticketC = getTicket();
+
+        System.out.println("Zona A: " + ticketA);
+        System.out.println("Zona B: " + ticketB);
+        System.out.println("Zona C: " + ticketC);
+    }
+
+    private static List<Integer> getTicket() {
         List<Integer> numbers = new ArrayList<>(
                 IntStream.rangeClosed(1,49)
                         .boxed()
@@ -17,7 +27,6 @@ public class LottoGenerator  {
                 .limit(6)
                 .sorted()
                 .toList();
-
-        System.out.println("Numerele câștigătoare 😄: " + ticket);
+        return ticket;
     }
 }
